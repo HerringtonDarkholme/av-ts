f02f6145c04209a8b05cc12302c996d54dae40a2
Herrington Darkholme <nonamesheep1@gmail.com>
2016-09-02 15:03:03 +0800

    Update definitions for renderFunc and internalPropertyKeys
    TS rocks!

--- src/interface.ts
+++ src/interface.ts
@@ -1,5 +1,6 @@
 import {ComponentOptions as ComponentOptions_} from 'vue/types/options'
 import {Vue} from 'vue/types/vue'
+import {VNode} from 'vue/types/vnode'
 
 export type Hash<V> = {[k: string]: V}
 export type VClass<T extends Vue> = {new(): T}
@@ -8,8 +9,21 @@ export interface DecoratorPorcessor {
   (proto: any, instance: Vue, options: ComponentOptions): void
 }
 
-export type $$Prop = string
+export type $$Prop = string & {'$$Prop Brand': never}
 
 export interface ComponentOptions extends ComponentOptions_ {
   [k: string]: any
 }
+
+interface ContextObject {
+  readonly props: any;
+  readonly children: VNode[];
+  readonly slots: any;
+  readonly data: any;
+  readonly parent: VNode;
+}
+export type RenderFunc = (
+  this: never,
+  h: typeof Vue.prototype.$createElement,
+  context: ContextObject
+) => VNode
