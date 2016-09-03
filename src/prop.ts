c56505524a104e182a1d7013d5dfba3c32c1cdd2
Herrington Darkholme <nonamesheep1@gmail.com>
2016-09-03 10:46:45 +0800

    implement Props

--- src/prop.ts
+++ src/prop.ts
@@ -1,19 +1,39 @@
 import Vue = require('vue')
-import {PropOptions, VClass, $$Prop} from './interface'
+import {ComponentOptions, PropOptions, VClass, $$Prop} from './interface'
 import {Component} from './core'
 
+import {getReflectType} from './util'
+
+type Props = {[k: string]: PropOptions}
 
 const PROP_KEY = '$$Prop' as $$Prop
 
 export function Prop(target: Vue, key: string): void
 export function Prop(config: PropOptions): PropertyDecorator
-export function Prop(target: Vue | PropOptions, key?: string): PropertyDecorator | void {
+export function Prop(target: any, key?: string): PropertyDecorator | void {
   if (key) {
+    let props: Props = target[PROP_KEY] = target[PROP_KEY] || {}
+    props[key] = {type: getReflectType(target, key)}
     return
   }
-  return function(cls, key) {
+  let config = target
+  return function(target: any, key: string) {
+    let props: any = target[PROP_KEY] = target[PROP_KEY] || {}
+    config.type = config.type || getReflectType(target, key)
+    props[key] = config
   }
 }
 
-Component.register(PROP_KEY, function() {
+Component.register(PROP_KEY, function(proto, instance, options) {
+  let props: Props = proto[PROP_KEY]
+  let inst: any = instance
+  for (let key in props) {
+    let prop = props[key]
+    if (inst[key] != null) {
+      // TODO, return a snapshot for object/array
+      prop.default = inst[key]
+      delete inst[key]
+    }
+  }
+  options.props = props
 })
